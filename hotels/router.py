940ddb26c1615a68303c4e7ddcbc7b4c657061f1
Alexandr Razdymakho <pavlechenk@gmail.com>
2024-03-05 17:29:10 +0300

    I enable redis caching

--- hotels/router.py
+++ hotels/router.py
@@ -1,20 +1,24 @@
 from datetime import date
-import asyncio
+from typing import Optional
+
 from fastapi import APIRouter
+from fastapi_cache.decorator import cache
 
 from app.hotels.dao import HotelDAO
+from app.hotels.services import HotelService
 from app.hotels.shemas import SHotel, SHotelInfo
 
-router = APIRouter(prefix="/hotels", tags=['Отели'])
+router = APIRouter(prefix="/hotels", tags=["Отели"])
 
 
-@router.get("", description='Возвращает список отелей по заданным параметрам')
-@cache(expire=20)
-async def get_hotels_by_location(location: str, date_from: date, date_to: date) -> list[SHotelInfo]:
-    await asyncio.sleep(3)
-    return await HotelDAO.find_all(location=location, date_from=date_from, date_to=date_to)
+@router.get("", description="Возвращает список отелей по заданным параметрам")
+@cache(expire=30)
+async def get_hotels_by_location(
+    location: str, date_from: date, date_to: date
+) -> list[SHotelInfo]:
+    return await HotelService.get_hotels_by_location(location, date_from, date_to)
 
 
-@router.get('/id/{hotel_id}', description='Возвращает все данные по одному отелю')
-async def get_hotel(hotel_id: int) -> SHotel:
-    return await HotelDAO.find_by_id(hotel_id)
+@router.get("/id/{hotel_id}", description="Возвращает все данные по одному отелю")
+async def get_hotel(hotel_id: int) -> Optional[SHotel]:
+    return await HotelDAO.find_one_or_none(id=hotel_id)
--- main.py
+++ main.py
@@ -1,26 +1,34 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
+
 
 from app.bookings.router import router as router_bookings
 from app.config import settings
-from app.users.router import router as router_users
-from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
-from app.pages.router import router as router_pages
+from app.hotels.router import router as router_hotels
 from app.images.router import router as router_images
+from app.pages.router import router as router_pages
+from app.users.router import router as router_users
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    redis = aioredis.from_url(f"{settings.REDIS_URL}")
+    FastAPICache.init(RedisBackend(redis), prefix="cache")
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.mount('/static', StaticFiles(directory='app/static'), 'static')
 
-app.include_router(router_users)
-app.include_router(router_bookings)
-app.include_router(router_hotels)
-app.include_router(router_rooms)
-app.include_router(router_pages)
-app.include_router(router_images)
-
+for router in [router_users, router_bookings, router_hotels, router_rooms, router_pages, router_images]:
+    app.include_router(router)
 
 origins = settings.ORIGINS.split(';')
 
