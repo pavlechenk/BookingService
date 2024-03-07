5d8bc09da3a2903d468fde3e26b11d25abf6b0e8
Alexandr Razdymakho <pavlechenk@gmail.com>
2024-03-07 18:15:38 +0300

    Adding attributes to connect to the test database

--- config.py
+++ config.py
@@ -1,19 +1,42 @@
+from typing import Literal
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    MODE: Literal['DEV', 'TEST', 'PROD']
+
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASSWORD: str
     DB_NAME: str
+
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASSWORD: str
+    TEST_DB_NAME: str
+
     ALGORITHM: str
     SECRET_KEY: str
+
     ORIGINS: str
+    REDIS_URL: str
+
+    EMAIL_HOST: str
+    EMAIL_PORT: int
+    EMAIL_USER: str
+    EMAIL_PASSWORD: str
 
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    @property
+    def TEST_DATABASE_URL(self):
+        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASSWORD}@{self.TEST_DB_HOST}:" \
+               f"{self.TEST_DB_PORT}{self.TEST_DB_NAME}"
 
     model_config = SettingsConfigDict(env_file='.env')
 
