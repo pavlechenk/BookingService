36fbcbabf08f3eaeeba89fe4f2a3ab498cb3265a
Alexandr Razdymakho <pavlechenk@gmail.com>
2024-03-07 17:30:05 +0300

    Refactoring pydantic schema, dao layer and router for rooms

--- hotels/rooms/dao.py
+++ hotels/rooms/dao.py
@@ -1,11 +1,11 @@
 from datetime import date
 
-from sqlalchemy import select, func
+from sqlalchemy import func, select
 
 from app.bookings.models import Bookings
 from app.dao.base import BaseDAO
-from app.hotels.rooms.models import Rooms
 from app.database import async_session_maker
+from app.hotels.rooms.models import Rooms
 
 
 class RoomDAO(BaseDAO):
@@ -13,13 +13,18 @@ class RoomDAO(BaseDAO):
 
     @classmethod
     async def find_all(cls, hotel_id: int, date_from: date, date_to: date):
-        async with async_session_maker() as session:
-            query = select(Rooms.__table__.columns,
-                    (Rooms.price * (date_to - date_from).days).label('total_cost'),
-                    (Rooms.quantity - func.count(Bookings.room_id)).label('rooms_left')
-            ).select_from(Rooms).join(
-                Bookings, Bookings.room_id == Rooms.id, isouter=True
-            ).where(Rooms.hotel_id == hotel_id).group_by(Rooms.id)
+        query = (
+            select(
+                Rooms.__table__.columns,
+                (Rooms.price * (date_to - date_from).days).label("total_cost"),
+                (Rooms.quantity - func.count(Bookings.room_id)).label("rooms_left"),
+            )
+            .select_from(Rooms)
+            .join(Bookings, Bookings.room_id == Rooms.id, isouter=True)
+            .where(Rooms.hotel_id == hotel_id)
+            .group_by(Rooms.id)
+        )
 
+        async with async_session_maker() as session:
             rooms = await session.execute(query)
             return rooms.mappings().all()
--- hotels/rooms/router.py
+++ hotels/rooms/router.py
@@ -1,21 +1,24 @@
-from datetime import date
+from datetime import date, datetime, timedelta
 
 from fastapi import APIRouter, Query
 
-from app.hotels.rooms.shemas import SRoomInfo
 from app.hotels.rooms.dao import RoomDAO
+from app.hotels.rooms.shemas import SRoomInfo
+
+router = APIRouter(prefix="/hotels", tags=["Отели"])
 
 
-router = APIRouter(
-    prefix='/hotels',
-    tags=['Отели']
+@router.get(
+    "/{hotel_id}/rooms",
+    description="Возвращает список всех номеров определенного отеля",
 )
-
-
-@router.get("/{hotel_id}/rooms", description='Возвращает список всех номеров определенного отеля')
 async def get_rooms(
-        hotel_id: int,
-        date_from: date = Query(default='2023-01-01'),
-        date_to: date = Query(default='2023-12-31')
+    hotel_id: int,
+    date_from: date = Query(
+        description=f"Например, {datetime.now().date()}"
+    ),
+    date_to: date = Query(
+        description=f"Например, {(datetime.now() + timedelta(days=14)).date()}",
+    ),
 ) -> list[SRoomInfo]:
     return await RoomDAO.find_all(hotel_id, date_from, date_to)
