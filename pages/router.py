d2c28145691dafc066e939364c4bb59505fa31e9
Alexandr Razdymakho <pavlechenk@gmail.com>
2024-03-07 17:38:23 +0300

    I am implementing a router to receive html pages about hotels

--- pages/router.py
+++ pages/router.py
@@ -1,19 +1,15 @@
-from fastapi import APIRouter, Request, Depends
+from fastapi import APIRouter, Depends, Request
 from fastapi.templating import Jinja2Templates
 
 from app.hotels.router import get_hotels_by_location
 
-router = APIRouter(
-    prefix='/pages',
-    tags=['Фронтенд']
-)
+router = APIRouter(prefix="/pages", tags=["Фронтенд"])
 
-templates = Jinja2Templates(directory='app/templates')
+templates = Jinja2Templates(directory="app/templates")
 
 
-@router.get('/hotels')
+@router.get("/hotels")
 async def get_hotels_page(request: Request, hotels=Depends(get_hotels_by_location)):
-    return templates.TemplateResponse(name='hotels.html', context={
-        'request': request,
-        'hotels': hotels
-    })
+    return templates.TemplateResponse(
+        name="hotels.html", context={"request": request, "hotels": hotels}
+    )
