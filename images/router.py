b12e0ca531d1fa154ca1b7bb701dcaeee47cb2f3
Alexandr Razdymakho <pavlechenk@gmail.com>
2024-03-07 17:58:51 +0300

    Implement a deferred call to the image processing task

--- images/router.py
+++ images/router.py
@@ -1,14 +1,16 @@
-from fastapi import APIRouter, UploadFile, status
 import aiofiles
+from fastapi import APIRouter, UploadFile, status
 
-router = APIRouter(
-    prefix='/images',
-    tags=['Загрузка картинок']
-)
+from app.tasks.tasks import process_picture
+
+router = APIRouter(prefix="/images", tags=["Загрузка картинок"])
 
 
-@router.post('/hotels', status_code=status.HTTP_201_CREATED)
+@router.post("/hotels", status_code=status.HTTP_201_CREATED)
 async def add_hotel_image(name: int, upload_file: UploadFile):
-    async with aiofiles.open(f'app/static/images/{name}.webp', 'wb+') as file_obj:
+    image_path = f"app/static/images/{name}.webp"
+    async with aiofiles.open(image_path, "wb+") as file_obj:
         file = await upload_file.read()
         await file_obj.write(file)
+
+    process_picture.delay(image_path)
