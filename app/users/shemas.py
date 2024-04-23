a6cd3e9b7e4e120708491546c37f0c9a22d9b666
Alexandr Razdymakho <pavlechenk@gmail.com>
2024-04-24 00:38:03 +0300

    Create user shema

--- app/users/shemas.py
+++ app/users/shemas.py
@@ -1,6 +1,17 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 
 
 class SUserAuth(BaseModel):
     email: EmailStr
     password: str
+
+
+class UserShema(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    is_active: bool = True
+    is_admin: bool = False
+    
+    
+    model_config = ConfigDict(from_attributes=True)
