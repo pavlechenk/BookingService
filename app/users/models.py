1602a8f9df532ea1e4c58be82ef036a68b15295a
Alexandr Razdymakho <pavlechenk@gmail.com>
2024-04-23 23:33:25 +0300

    add new columns in user table and create migration

--- app/users/models.py
+++ app/users/models.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-
+from sqlalchemy import Boolean
 from app.database import Base
 
 if TYPE_CHECKING:
@@ -10,8 +10,11 @@ if TYPE_CHECKING:
 
 class Users(Base):
 
-    email: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
+    username: Mapped[str] = mapped_column(unique=True)
     hashed_password: Mapped[str]
+    is_active: Mapped[bool] = mapped_column(default=True, server_default="true")
+    is_admin: Mapped[bool] = mapped_column(default=False, server_default="false")
 
     bookings: Mapped[list["Bookings"]] = relationship(back_populates="user")
 
